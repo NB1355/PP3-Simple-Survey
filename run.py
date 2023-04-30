664f2dd9a3290c633f185ec924d1824a9f48e35f
NB1355 <nahal5513@gmail.com>
2023-04-30 16:33:57 +0000

    TST Test opening a web page in a the

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+# import urllib.request
+import webbrowser
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,3 +19,13 @@ SHEET = GSPREAD_CLIENT.open('PP3-data-sets')
 data = (SHEET.worksheet('instruction')).get_all_values()
 
 print(data)
+
+
+
+# get_url= urllib.request.urlopen('https://www.google.com/')
+
+# print("Response Status: "+ str(get_url.getcode()) )
+# print(get_url.read())
+
+get_url= webbrowser.open('https://nb1355.github.io/PP1-Mountaineering-Global/',2)
+
