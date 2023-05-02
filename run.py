e6ad738772591afb6e8ea272e32b99ec9bfbd970
NB1355 <nahal5513@gmail.com>
2023-05-02 00:42:36 +0000

    ADD Setup structure for executing functions based on the user input, reading from json file as variables TST Pass

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
+
 import gspread
 from google.oauth2.service_account import Credentials
-# import urllib.request
-import webbrowser
+import json
+import pandas as pd
 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -16,18 +17,51 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('PP3-data-sets')
 
 
-data = (SHEET.worksheet('instruction')).get_all_values()
+def instructions():
+    # Opening JSON file
+    with open('setings.json') as json_file:
+        data = json.load(json_file)
+    options = data['options']
 
-print(data)
+    # load data into a DataFrame object:
+    df = pd.DataFrame(options).set_index('Op')
+    dfp = df.drop('id', axis=1)
+
+    print(dfp.to_string(header=False))
+    print('------------------------------------')
 
 
+def f_00():
+    print(' ... execute function 0')
 
-# get_url= urllib.request.urlopen('https://www.google.com/')
+def f_01():
+    print(' ... execute function 1')
 
-# print("Response Status: "+ str(get_url.getcode()) )
-# print(get_url.read())
+def f_02():
+    print(' ... execute function 2')
 
-get_url= webbrowser.open('https://nb1355.github.io/PP1-Mountaineering-Global/',2)
+def f_03():
+    print(' ... execute function 3')
 
-print ('Auomated depoyment in heroko is working')
+def options_run():
+    #get the user input and executs functions based on the user input
+    instructions()
+    op = int(input('Your option: '))
+    match op:
+        case 0:
+            f_00()  
+        case 1:
+            f_01()
+        case 2:
+            f_02()
+        case 3:
+            f_03()
 
+
+def main(): 
+    print('\nW E L C O M E ! \nChoose form options below to proceed...')
+    options_run()
+
+
+    print('\nD O N E ! \n                                    python3 run.py')
+main()
